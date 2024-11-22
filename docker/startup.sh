580a1a7af01961ee1072e223ef69268cd7ae617b
MartaRende <121174493+MartaRende@users.noreply.github.com>
2024-11-22 12:49:30 +0100

    wip

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-Xvfb :99 -ac -screen 0 "$XVFB_RES" -nolisten tcp $XVFB_ARGS &
+Xvfb :99 -ac -screen 0 "$XVFB_RES" -nolisten tcp &
 XVFB_PROC=$!
-sleep 1
-export DISPLAY=:99
+sleep 2
+echo $DISPLAY
 python3 scripts/main.py
 kill $XVFB_PROC
