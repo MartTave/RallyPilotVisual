5b8ffcdf2e10f318b9c3c9e17ee7a8c4d80dd51b
MartaRende <121174493+MartaRende@users.noreply.github.com>
2024-12-02 17:15:39 +0100

    wip

--- data_tools/symetric.py
+++ data_tools/symetric.py
@@ -2,26 +2,24 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-BASEPATH = "data/record_norm"
+BASEPATH = ["data/train", "data/test"]
 BASE_EXTENSION = ".npz"
-files = [4, 5, 6, 7]
 
-for f in files:
-    newPath = f"{BASEPATH}{str(f)}_noise_flipped{BASE_EXTENSION}"
-    if os.path.exists(newPath):
-        print("Found ", newPath, " skipping...")
-        continue
-    fullPath = BASEPATH + str(f) + "_noise" + BASE_EXTENSION
-    data = np.load(fullPath)
-    images = data["images"]
-    controls = data["controls"]
-    speeds = data["speeds"]
-    distances = data["distances"]
-    images = np.flip(images, axis=3)
-    np.savez(
-        newPath,
-        images=images,
-        controls=controls,
-        speeds=speeds,
-        distances=distances
-    )
+for b in BASEPATH:
+    for f in os.listdir(b):
+        path = f"{b}/{f}"
+        if not os.path.isfile(path):
+            continue
+        data = np.load(path)
+        images = data["images"]
+        controls = data["controls"]
+        speeds = data["speeds"]
+        distances = data["distances"]
+        images = np.flip(images, axis=3)
+        np.savez(
+            f"{b}/{f.split('.')[0]}_flipped.npz",
+            images=images,
+            controls=controls,
+            speeds=speeds,
+            distances=distances
+        )
