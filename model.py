987b08614e3e4a6ed605c8043c934a49c61553aa
marttave <martin.tavernier256@gmail.com>
2024-12-05 14:48:08 +0000

    Changed model size

--- model.py
+++ model.py
@@ -11,13 +11,13 @@ class AlexNetAtHome(nn.Module):
     def __init__(self):
         super(AlexNetAtHome, self).__init__()
 
-        firstLayerSize = 64
-        secondLayerSize = 75
-        thirdLayerSize = 90
-        fourthLayerSize = 90
-        fifthLayerSize = 75
+        firstLayerSize = 32
+        secondLayerSize = 64
+        thirdLayerSize = 80
+        fourthLayerSize = 80
+        fifthLayerSize = 64
         # Calculate the input size of the predictor layer, this is the size of the last layer of the features flattenend
-        predictorInputSize = 2700
+        predictorInputSize = 2304
 
         # Maybe diminish stride -> reduce kernel size and
 
@@ -44,12 +44,6 @@ class AlexNetAtHome(nn.Module):
         self.predictor = nn.Sequential(
             nn.Dropout(0.5),
             nn.Linear(predictorInputSize, 256),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(256, 256),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(256, 256),
             nn.ReLU(),
             nn.Dropout(0.5),
             nn.Linear(256, 256),
