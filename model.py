83b0072b9a07343edc3fb0ce1274c726e70a3e0a
marttave <martin.tavernier256@gmail.com>
2024-12-05 15:02:13 +0000

    Reduced model size

--- model.py
+++ model.py
@@ -17,7 +17,7 @@ class AlexNetAtHome(nn.Module):
         fourthLayerSize = 40
         fifthLayerSize = 25
         # Calculate the input size of the predictor layer, this is the size of the last layer of the features flattenend
-        predictorInputSize = 2304
+        predictorInputSize = 900
 
         # Maybe diminish stride -> reduce kernel size and
 
